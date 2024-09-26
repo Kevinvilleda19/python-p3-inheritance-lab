c1da4c1a9049946ba9080f0a8dfc6a82fddb0bca
Kevin <kvilleda10566@gmail.com>
2024-09-26 19:01:51 -0400

    Done

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,26 @@ from user import User
 import random
 
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        # Initialize the knowledge list
+        self.knowledge = Teacher.knowledge
 
     def teach(self):
-        pass
+        # Get a random index between 0 and the last index of self.knowledge
+        random_index = random.randint(0, len(self.knowledge) - 1)
+        # Return the random knowledge string
+        return self.knowledge[random_index]
+
+
